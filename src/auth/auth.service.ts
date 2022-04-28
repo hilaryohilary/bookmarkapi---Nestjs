ff5f1c23d40ee49b2f559fb545a840fa5348ef41
hilaryohilary <hillaryononiwu@gmail.com>
2022-04-28 18:58:58 +0100

    installed and initialized jwt  passport and jwtstrategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from "../prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private Jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     // generate the password hash
@@ -51,8 +57,25 @@ export class AuthService {
     if (!pwMatches) {
       throw new ForbiddenException("Invalid credentials!");
     }
-    // send back the user
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get("JWT_SECRET");
+    const token = await this.Jwt.signAsync(payload, {
+      expiresIn: "15m",
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
